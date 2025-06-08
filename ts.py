c8dd543faa4ea065febbfee26ed7e3f1ef84f372
mehdisheyxi <mehdisheyxi@gmail.com>
2025-06-08 03:38:41 -0400

    add file

---
+++ ts.py
@@ -0,0 +1,21 @@
+# import unittest
+#
+#
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)  # add assertion here
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+pc = 'hamed'
+dic_val = {
+    'no1': 'hamed',
+    'no2':'nehdi'
+}
+
+no = 'no1'
+if pc == dic_val[no]:
+    print('yes')
+else:
+    print('no')
